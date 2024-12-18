c6b29bb221cbd9700906842623d5a9c49834b1e7
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2024-12-18 11:34:13 +0200

    1.0.5

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -6,16 +6,50 @@ contract ERC721 is IERC721Metadata {
   string public name;
   string public symbol;
   mapping(address => uint) _balances;
-  mapping(uint => address) _owner;
+  mapping(uint => address) _owners;
   mapping(uint => address) _tokenApprovals;
   mapping(address => mapping(address => bool)) _operatorApprovals;
 
   modifier _requireMinted(uint tokenId) {
-    require(_exits());
+    require(_exists(tokenId), "not minted");
+    _;
   }
 
   constructor(string memory _name, string memory _symbol) {
     name = _name;
     symbol = _symbol;
   }
+
+  function transferFrom(address from, address to, uint tokenId) external {
+    require(_isApprovedOrOwner(msg.sender, tokenId), "not an owner or approved!");
+    _transfer(from, to, tokenId);
+  }
+
+  function safeTransferFrom(address from, address to, uint tokenId) external {
+
+  }
+
+  function ownerOf(uint tokeId) public view _requireMinted(tokenId) returns(address) {
+    return _owners[tokenId];
+  }
+
+  function _isApprovedForAll(address owner, address operator) external view returns(bool) {
+    
+  }
+
+  function _isApprovedOrOwner(address spender, uint tokenId) internal view returns(bool) {
+    address owner = ownerOf(tokenId);
+
+    require(
+      spender == owner ||
+      isApprovedForAll(owner, spender) ||
+      getApproved(tokenId) == spender,
+      "not an owner or approved!"
+    );
+  }
+
+  function _exists(uint tokenId) internal view returns(bool) {
+    return _owners[tokenId] != address(0);
+  }
+
 }

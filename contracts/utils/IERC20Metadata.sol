815fc9b9be18b293126439f740bfe4378a458c81
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-08-06 14:48:28 +0300

    2.1.4

--- contracts/utils/IERC20Metadata.sol
+++ contracts/utils/IERC20Metadata.sol
@@ -4,7 +4,7 @@ import "./IERC20.sol";
 
 interface IERC20Metadata is IERC20 {
   function name() external view returns(string memory);
-
+  
   function symbol() external view returns(string memory);
 
   function decimals() external view returns(uint8);

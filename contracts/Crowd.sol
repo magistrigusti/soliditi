abf205228ee8c66c50621865b4e872ee11d23edf
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-08-24 17:02:22 +0300

    2.3.1

--- contracts/Crowd.sol
+++ contracts/Crowd.sol
@@ -19,7 +19,12 @@ contract Crowd {
   uint public constant MAX_DURATION = 100 days;
   uint public constant MIN_DURATION = 10;
 
-  event Launched(currentId, msg.sender, _goal, _startAt, _endAt);
+  event Launched(uint Id, address owner, uint goal, uint startAt, uint endAt);
+  event Cancel(uint id);
+  event Pledged(uint id, address pledger, uint amount);
+  event Unpledged(uint id, address pledger, uint amount);
+  event Claimed(uint id);
+  event Refunded(uint id, address pledger, uint amount); 
 
   constructor(address _token) {
     token = IERC20(_token);
@@ -41,5 +46,58 @@ contract Crowd {
 
     emit Launched(currentId, msg.sender, _goal, _startAt, _endAt);
     currentId += 1;
+  }
+
+  function cancel(uint _id) external {
+    Campaign memory campaign = campaigns[_id];
+    require(msg.sender == campaign.owner, "not an owner");
+    require(block.timestamp < campaign.startAt, "already started!");
+
+    delete campaigns[_id];
+    emit Cancel(_id);
+  }  
+
+  function pledge(uint _id, uint _amount) external {
+    Campaign storage campaign = campaigns[_id];
+    require(block.timestamp >= campaign.startAt, "not started!");
+    require(block.timestamp < campaign.endAt, "ended");
+
+    campaign.pledged += _amount;
+    pledges[_id][msg.sender] += _amount;
+    token.transferFrom(msg.sender, address(this), _amount);
+    emit Pledged(_id, msg.sender, _amount);
+  }
+
+  function unpledge(uint _id, uint _amount) external {
+    Campaign storage campaign = campaigns[_id];
+    require(block.timestamp < campaign.endAt, "ended");
+
+    campaign.pledged -= _amount;
+    pledges[_id][msg.sender] -= _amount;
+    token.transfer(msg.sender, _amount);
+    emit Unpledged(_id, msg.sender, _amount);
+  }
+
+  function claim(uint _id) external {
+    Campaign storage campagn = campaigns[_id];
+    require(msg.sender == campaign.owner, "not an owner");
+    require(block.timestamp > campaign.endAt, "not ended");
+    require(campaign.pledged >= campaign.goal, "pledged is too low");
+    require(!campaign.claimed, "already claimed");
+
+    campaing.claimed = true;
+    token.transfer(msg.sender, campaing.pledged);
+    emit Claimed(_id);
+  }
+
+  function refund(uint _id) external {
+    Campaing storage campaign = campaigns[_id];
+    require(block.timestamp > campaign.endAt, "not ended");
+    require(campaign.pledged < campaign.goal, "reached gaol");
+
+    uint pledgedAmmount = pledges[_id][msg.sender];
+    pledges[_id][msg.sender] = 0;
+    token.transfer(_id, msg.sender, pledgedAmount);
+    emit Refunded(_id, msg.sender, pledgedAmount);
   }
 }
